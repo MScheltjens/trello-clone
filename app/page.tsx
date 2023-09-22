023044e68a50e6257abcd78abee9fbd9cdc62b7b
Mathias Scheltjens <mathias.scheltjens@gmail.com>
2023-09-22 11:02:34 +0200

    h1 typo fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main>
-      <h1>Trellop 2.O AI Clone</h1>
+      <h1>Trello 2.O AI Clone</h1>
     </main>
   );
 }
