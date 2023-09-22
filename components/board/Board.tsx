1aa782e719b23e8839aa7b316414ccc836bd8a78
Mathias Scheltjens <mathias.scheltjens@gmail.com>
2023-09-22 17:01:14 +0200

    db update up

--- components/board/Board.tsx
+++ components/board/Board.tsx
@@ -7,7 +7,7 @@ import { Column } from '../column/Column';
 import { IColumn } from '@/typings';
 
 export const Board = () => {
-  const [board, getBoard, setBoardState] = useBoardStore((state) => [state.board, state.getBoard, state.setBoardState]);
+  const [board, getBoard, setBoardState, updateTodoDB] = useBoardStore((state) => [state.board, state.getBoard, state.setBoardState, state.updateTodoDB]);
 
   useEffect(() => {
     getBoard();
@@ -72,7 +72,7 @@ export const Board = () => {
       newColumns.set(startCol.id, newCol);
       newColumns.set(endCol.id, { id: endCol.id, todos: finishTodos });
 
-      // update in DB
+      updateTodoDB(todoMoved, endCol.id);
 
       setBoardState({ ...board, columns: newColumns });
     }
--- store/boardStore.ts
+++ store/boardStore.ts
@@ -1,11 +1,13 @@
+import { databases } from '@/appWrite';
 import { getTodosGroupedByColumn } from '@/lib';
-import { IBoard, IColumn, TTypedColumn } from '@/typings';
+import { IBoard, IColumn, ITodo, TTypedColumn } from '@/typings';
 import { create } from 'zustand';
 
 interface IBoardState {
   board: IBoard;
   getBoard: () => void;
   setBoardState: (board: IBoard) => void;
+  updateTodoDB: (todo: ITodo, columnId: string) => void;
 }
 
 export const useBoardStore = create<IBoardState>((set) => ({
@@ -17,4 +19,10 @@ export const useBoardStore = create<IBoardState>((set) => ({
     set({ board });
   },
   setBoardState: (board) => set({ board }),
+  updateTodoDB: async (todo, columnId) => {
+    await databases.updateDocument(process.env.NEXT_PUBLIC_DATABASE_ID!, process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!, todo.$id, {
+      title: todo.title,
+      status: columnId,
+    });
+  },
 }));
