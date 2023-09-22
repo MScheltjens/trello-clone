411dd9836b3c06890f3184b7d50cc2d3970d163e
Mathias Scheltjens <mathias.scheltjens@gmail.com>
2023-09-22 11:54:16 +0200

    ready for first deploy

--- components/board/Board.tsx
+++ components/board/Board.tsx
@@ -2,10 +2,11 @@ import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 export const Board = () => {
   return (
-    <DragDropContext>
-      <Droppable droppableId="board" direction="horizontal" type="column">
-        {(provided) => <div>{/*rendering all columns*/}</div>}
-      </Droppable>
-    </DragDropContext>
+    <></>
+    // <DragDropContext>
+    //   <Droppable droppableId="board" direction="horizontal" type="column">
+    //     {(provided) => <div>{/*rendering all columns*/}</div>}
+    //   </Droppable>
+    // </DragDropContext>
   );
 };
