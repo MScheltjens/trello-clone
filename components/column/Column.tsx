c92ab156b7d08f81367975c80d1fa8712cbd4ea6
Mathias Scheltjens <mathias.scheltjens@gmail.com>
2023-09-22 15:24:39 +0200

    h2

--- components/column/Column.tsx
+++ components/column/Column.tsx
@@ -25,7 +25,10 @@ export const Column = ({ id, todos, index }: Props) => {
           <Droppable droppableId={index.toString()} type="card">
             {(provided, snapShot) => (
               <div {...provided.droppableProps} ref={provided.innerRef} className={`p-2 rounded-2xl shadow-sm ${snapShot.isDraggingOver ? 'bg-green-200' : 'bg-white/50'}`}>
-                <h2>{id}</h2>
+                <h2 className="flex justify-between font-bold text-xl p-2">
+                  {idToColumnText[id]}
+                  <span className="text-gray-500 bg-gray-200 rounded-full px-2 py-1 text-sm font-normal">{todos.length}</span>
+                </h2>
               </div>
             )}
           </Droppable>
